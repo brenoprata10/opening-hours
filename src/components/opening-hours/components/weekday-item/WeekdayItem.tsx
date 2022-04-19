6ba5eb1a1f7e3993b1da5e1acbfd58da76790ee6
Breno Prata <brenoprata@hotmail.com>
2022-04-19 16:43:05 +0200

    Handle responsive behavior for opening hour label

--- src/components/opening-hours/components/weekday-item/WeekdayItem.tsx
+++ src/components/opening-hours/components/weekday-item/WeekdayItem.tsx
@@ -1,8 +1,11 @@
 import styles from 'components/opening-hours/components/weekday-item/WeekdayItem.module.scss'
 import EHourOperationType from 'enums/EHourOperation'
 import EWeekday from 'enums/EWeekday'
+import React from 'react'
 import TMerchantHourOperation from 'types/TMerchantHourOperation'
 import {getFormattedTime} from 'utils/time'
+
+const HOUR_SEPARATOR = '-'
 
 const WeekdayItem = ({
 	weekday,
@@ -17,17 +20,24 @@ const WeekdayItem = ({
 
 	return (
 		<div className={styles.wrapper}>
-			<div>
-				<span className={styles.weekday}>{weekday}</span>
+			<div className={styles.weekday}>
+				<span className={styles['weekday-label']}>{weekday}</span>
 				{displayTodayTag && <b className={styles['today-tag']}>TODAY</b>}
 			</div>
 			{isClosed ? (
 				<div className={styles.closed}>Closed</div>
 			) : (
-				<span>{openingDayHours.map((hourOperation) => getFormattedTime(hourOperation.value))}</span>
+				<div className={styles['opening-hour']}>
+					{openingDayHours.map(({type, value}) => (
+						<span>
+							{type === EHourOperationType.CLOSE && <>&nbsp;{HOUR_SEPARATOR}&nbsp;</>}
+							{getFormattedTime(value)}
+						</span>
+					))}
+				</div>
 			)}
 		</div>
 	)
 }
 
-export default WeekdayItem
+export default React.memo(WeekdayItem)
